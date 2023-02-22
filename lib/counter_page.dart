eb4997c6d24cb24a51e4ce75e43d1e594a1ade51
Ravi Singh Lodhi <ravisinghlodhi2357@gmail.com>
2023-02-22 10:35:06 +0530

    Make a counter app using ProxyProvider0

---
+++ lib/counter_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CounterPage extends StatelessWidget {
+  const CounterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final count = Provider.of<int>(context);
+    return Text(count.toString());
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:provider_tutorial/providers/counter_provider.dart';
+import 'package:provider_tutorial/counter_page.dart';
 
 void main() {
   runApp(
-    Provider(
-      create: (context) => const Counter(0),
-      child: const MainApp(),
-    ),
+    const MainApp(),
   );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final count = Provider.of<Counter>(context).count.toString();
+  State<MainApp> createState() => _MainAppState();
+}
 
+class _MainAppState extends State<MainApp> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text(count),
+          child: ProxyProvider0<int>(
+            update: (context, _) => count,
+            child: const CounterPage(),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              count++;
+            });
+          },
+          child: const Icon(Icons.add),
         ),
       ),
     );
